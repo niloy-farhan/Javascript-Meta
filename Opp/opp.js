2b61dfbfc116bab97a7775e98ebe6b3537c14702
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-30 15:11:50 +0600

    add some code

--- Opp/opp.js
+++ Opp/opp.js
@@ -8,3 +8,14 @@ var purchase1 = {
 }
 
 purchase1.totalPrice();
+
+var purchase2 = {
+    shoes: 50,
+    stateTax: 1.2,
+    totalPrice: function () {
+        var calculation = this.shoes * this.stateTax;
+        console.log(`Total price: ${calculation}`);
+    }
+}
+
+purchase2.totalPrice();
